e9e8aca09560f09d61ec0f74eed03549f0862982
JOSEEAND <115430851+JOSEEAND@users.noreply.github.com>
2024-02-19 18:31:47 -0600

    avances3

--- src/views/index.js
+++ src/views/index.js
@@ -1,43 +1,39 @@
 import React from 'react';
-import { Image, ScrollView,  Text, View} from 'react-native';
-import {style_01} from '../styles/style_01';
+import { Image, ScrollView, Text, View } from 'react-native';
+import { style_01 } from '../styles/style_01';
 
-		const data = [
-			{name:'Bulbasaur',
-            image: require('../img/celebi.png')},
-            {name:'Ivysaur',
-            image: require('../img/turtwig.png')},
-            {name:'Venusaur',
-            image: require('../img/victini.png')},
-            {name:'Charmander',
-            image: require('../img/charender.png')},
-            {name:'Charmeleon',
-            image: require('../img/charmaleon.png')},
-            {name:'Charizard',
-            image: require('../img/charizard.png')}
-		];
+const data = [
+  { name: 'Bulbasaur', image: require('../img/bulbasaur.jpeg') },
+  { name: 'Ivysaur', image: require('../img/ivisaur.jpeg') },
+  { name: 'Venusaur', image: require('../img/venusaur.jpeg') },
+  { name: 'Charmander', image: require('../img/charender.png') },
+  { name: 'Charmeleon', image: require('../img/charmaleon.png') },
+  { name: 'Charizard', image: require('../img/charizard.png') }
+];
 
-		const Index = () => {
-			return(
-				<View>
-					<View style={style_01.divHeader}>
-						<Image source={require('../img/logoPM.png')} />
-					</View>
+const Index = () => {
+  return (
+    <View>
+      <View style={style_01.divHeader}>
+        <Image source={require('../img/logoPM.png')} />
+      </View>
 
-					<View style={style_01.divMain}>
-						<Text style={style_01.p1}>Trainer</Text>
-						<Text style={style_01.p1}>Teams</Text>
-                        <Text style={style_01.h1}>First generation</Text>
+      <View style={style_01.divMain}>
+        <Text style={style_01.p1}>Trainer</Text>
+        <Text style={style_01.p1}>Teams</Text>
+        <Text style={style_01.h1}>First generation</Text>
 
-						<ScrollView style={{marginTop:8}}>
-							{data.map(item => <View style={style_01.tarjeta}>
-								<Text>{item.name}</Text>
-								<Text>{item.image}</Text>
-							</View>)}
-						</ScrollView>
-					</View>
-				</View>
-			);
-		}
+        <ScrollView style={{ marginTop: 8 }}>
+          {data.map(item => (
+            <View style={style_01.tarjeta} key={item.name}>
+              <Text>{item.name}</Text>
+              <Image source={item.image} style={{ width: 50, height: 50 }} />
+            </View>
+          ))}
+        </ScrollView>
+      </View>
+    </View>
+  );
+};
 
 export default Index;

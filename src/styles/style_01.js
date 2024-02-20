d156a483d4517bc77502971b22834f26fb654d74
JOSEEAND <115430851+JOSEEAND@users.noreply.github.com>
2024-02-19 18:26:03 -0600

    avances2

--- src/styles/style_01.js
+++ src/styles/style_01.js
@@ -2,13 +2,11 @@ import { Dimensions, StyleSheet} from 'react-native';
 
 		const alto = Dimensions.get('window').height - 150
 
-		// crea paleta de colores
 		const principal = '#1B2E66';
 		const naranja = '#FF9900';
 		const blanco = '#FFFFFF';
 		const gris_1 = '#B2BDD5';
 
-		// crea la hoja de estilos
 		export const style_01 = StyleSheet.create({
 			body:{
 				flex: 1,
@@ -18,7 +16,8 @@ import { Dimensions, StyleSheet} from 'react-native';
 				backgroundColor : principal,
 				flexDirection: 'row',
 				flexWrap: 'wrap',
-				height: 80,
+				height: 130,
+                marginLeft:65
 			},
 			divMain:{
 				backgroundColor: gris_1,
@@ -37,8 +36,9 @@ import { Dimensions, StyleSheet} from 'react-native';
 				fontWeight: '300',
 			},
 			h1:{
-				color: principal,
+				color:'gray',
 				marginTop:5,
+                textAlign:'center',
 				marginBottom: 5,
 				fontSize: 24,
 				fontWeight: 'bold',
@@ -60,11 +60,11 @@ import { Dimensions, StyleSheet} from 'react-native';
 			p1:{
 				color: principal,
 				textAlign: 'justify',
-				lineHeight: 14,
-				fontSize: 12,
+				lineHeight: 17,
+				fontSize: 15,
 			},
 			tarjeta:{
-				backgroundColor: blanco,
+				backgroundColor: 'white',
 				borderRadius: 10,
 				margin: 5,
 				padding: 10,
--- src/views/index.js
+++ src/views/index.js
@@ -1,78 +1,43 @@
 import React from 'react';
-		import { Image, ScrollView,  Text, View} from 'react-native';
-		import {style_01} from '../styles/style_01';
+import { Image, ScrollView,  Text, View} from 'react-native';
+import {style_01} from '../styles/style_01';
 
 		const data = [
-			{codigo : 'ITI-121',
-				nombre : 'Fundamentos de Programación',
-				nivel  : 1,
-				creditos : 6},
-			{codigo : 'ITI-122',
-				nombre : 'Comunicación Oral y Escrita',
-				nivel  : 1,
-				creditos : 2},
-			{codigo : 'ITI-123',
-				nombre : 'Fundamentos de Bases de Datos',
-				nivel  : 1,
-				creditos : 3},
-			{codigo : 'ITI-124',
-				nombre : 'Matemáticas para Computación I',
-				nivel  : 1,
-				creditos : 6},
-			{codigo : 'ID-TEC-1',
-				nombre : 'Inglés 1',
-				nivel  : 1,
-				creditos : 0},
-			{codigo : 'IIT-221',
-				nombre : 'Programación I',
-				nivel  : 2,
-				creditos : 6},
-			{codigo : 'IIT-222',
-				nombre : 'Plataformas Tecnológicas I',
-				nivel  : 2,
-				creditos : 3},
-			{codigo : 'IIT-223',
-				nombre : 'Administración de Bases de Datos',
-				nivel  : 2,
-				creditos : 3},
-			{codigo : 'IIT-224',
-				nombre : 'Matemáticas para Computación II',
-				nivel  : 2,
-				creditos : 3},
-			{codigo : 'ID-TEC-2',
-				nombre : 'Inglés 1',
-				nivel  : 2,
-				creditos : 0}
-				
+			{name:'Bulbasaur',
+            image: require('../img/celebi.png')},
+            {name:'Ivysaur',
+            image: require('../img/turtwig.png')},
+            {name:'Venusaur',
+            image: require('../img/victini.png')},
+            {name:'Charmander',
+            image: require('../img/charender.png')},
+            {name:'Charmeleon',
+            image: require('../img/charmaleon.png')},
+            {name:'Charizard',
+            image: require('../img/charizard.png')}
 		];
 
 		const Index = () => {
 			return(
 				<View>
 					<View style={style_01.divHeader}>
-						<Image source={require('../imgs/logos/logo_universidad.png')} />
+						<Image source={require('../img/logoPM.png')} />
 					</View>
 
 					<View style={style_01.divMain}>
-						<Text style={style_01.h1}>Demostración 01</Text>
-						<Text style={style_01.p1}>Desde el 2019, se está realizando el proceso de rediseño de la carrera, el cual implica la actualización de los contenidos en las asignaturas, así como las formas de mediación mas actuales, todo esto tomando en cuenta tambien diversas recomendaciones derivadas del proceso de autoevalación y acreditación de la carrera.</Text>
+						<Text style={style_01.p1}>Trainer</Text>
+						<Text style={style_01.p1}>Teams</Text>
+                        <Text style={style_01.h1}>First generation</Text>
 
 						<ScrollView style={{marginTop:8}}>
-							{data.map(item => <View style={style_01.tarjeta} key={item.codigo} >
-								<Text>Código: {item.codigo}</Text>
-								<Text>Nombre: {item.nombre}</Text>
-								<Text>Nivel: {item.nivel}</Text>
-								<Text>Créditos: {item.creditos}</Text>
+							{data.map(item => <View style={style_01.tarjeta}>
+								<Text>{item.name}</Text>
+								<Text>{item.image}</Text>
 							</View>)}
 						</ScrollView>
-					</View>
-
-					<View style={style_01.divFooter}>
-						<Text style={style_01.textFooter}>Carrera de Tecnologías de Información</Text>
-						<Text style={style_01.textFooter}>Sede del Pacífico</Text>
 					</View>
 				</View>
 			);
 		}
 
-		export default Index;
+export default Index;
